df745ebc1c673381a63c912f2a569afc17f9c80b
padmini v <padmini.v@perfios.com>
2021-10-21 01:16:29 +0530

    REFACTORED: added reference to server_url

--- js/site_properties.js
+++ js/site_properties.js
@@ -1,4 +1,5 @@
 let site_properties = {
     home_page: "../pages/address_book_homepage.html",
-    add_contacts_page: "../pages/address_book_form.html"
+    add_contacts_page: "../pages/address_book_form.html",
+    server_url: "http://localhost:3000/addressBookContactDB/"
 };
