d13faec0302b869fc31531da9cc9ab4f2325c9dd
padmini v <padmini.v@perfios.com>
2021-10-21 03:55:18 +0530

    REFACTORED: changed variable name

--- js/utility.js
+++ js/utility.js
@@ -4,39 +4,41 @@ const ADDRESS_REGEX = RegExp('^[a-zA-Z0-9#@*(),.&\\s]{3,}$');
 const ZIP_REGEX = RegExp("^[0-9]{3}\\s{0,1}[0-9]{3}$");
 
 const checkName = (name) => {
-  if (!NAME_REGEX.test(name)) {
-      throw "NAME is Invalid!";
-  }
-};
-
-const checkPhoneNumber = (phoneNumber) => {
-    if (!PHONE_NUMBER_REGEX.test(phoneNumber)) {
-      throw "PHONE NUMBER is Invalid!";
+    if (!NAME_REGEX.test(name)) {
+        throw "NAME is Invalid!";
     }
-};
-
-const checkAddress = (address) => {
-  if (!ADDRESS_REGEX.test(address)) {
-    throw "ADDRESS is Invalid!";
-  }
-};
-
-const checkZip = (zip) => {
-    if (!ZIP_REGEX.test(zip)) {
-      throw "ZIPCODE is Invalid!";
+  };
+  
+  const checkPhoneNumber = (phoneNumber) => {
+      if (!PHONE_NUMBER_REGEX.test(phoneNumber)) {
+        throw "PHONE NUMBER is Invalid!";
+      }
+  };
+  
+  const checkAddress = (address) => {
+    if (!ADDRESS_REGEX.test(address)) {
+      throw "ADDRESS is Invalid!";
     }
-};
-
-function makeServiceCall(methodType, url, async = true, data = null) {
+  };
+  
+  const checkZip = (zip) => {
+      if (!ZIP_REGEX.test(zip)) {
+        throw "ZIPCODE is Invalid!";
+      }
+  };
+  
+  function makeServiceCall(methodType, url, async = true, data = null) {
     return new Promise(function (resolve, reject) {
       let xhr = new XMLHttpRequest();
       xhr.onload = function () {
         console.log(methodType+" state changed called. Ready state: "+xhr.readyState+" ,status: "+xhr.status);
         if (xhr.readyState == 4) {
-          if (xhr.status === 200 || xhr.status === 201) {
+          if (xhr.status === 200 || xhr.status === 201) 
+          {
             resolve(xhr.responseText);
           }
-          else if (xhr.status >= 400) {
+          else if (xhr.status >= 400) 
+          {
             reject({
               status: xhr.status,
               statusText: xhr.statusText
