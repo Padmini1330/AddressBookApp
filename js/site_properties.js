36e1e0c742f6ef3662ceefd76b7375cd877dd8d2
padmini v <padmini.v@perfios.com>
2021-10-21 01:30:16 +0530

    REFACTORED: changed server url

--- js/site_properties.js
+++ js/site_properties.js
@@ -1,5 +1,6 @@
 let site_properties = {
+    use_local_storage: "false",
     home_page: "../pages/address_book_homepage.html",
     add_contacts_page: "../pages/address_book_form.html",
-    server_url: "http://localhost:3000/addressBookContactDB/"
+    server_url: "http://localhost:3000/ContactDB/"
 };
